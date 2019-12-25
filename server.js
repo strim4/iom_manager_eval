9e5ef0bbb39795bd3949fba4ef3339dd1feb674e
strim4 <moritz_strickler@msn.com>
2019-12-25 14:09:20 +0100

    update mongodb url + app port

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(passport.initialize());
 
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost/iom_manager', function() {
+mongoose.connect('mongodb+srv://iommanager:Sommer2019@cluster0-vjmex.mongodb.net/test?retryWrites=true&w=majority', function() {
 console.log('Connection has been made');
 })
 .catch(err => {
@@ -129,7 +129,7 @@ function isLoggedIn(req, res, next) {
 };
 
 //Tell express server to use port 8081
-const port = process.env.API_PORT || 8081;
+const port = process.env.PORT || 8081;
 app.use('/', router);
 app.listen(port, function() {
 console.log(`api running on port ${port}`);
